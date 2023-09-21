a53b98c254038b083a3161c15b5fe350314bf900
c0de4un <code4un@yandex.ru>
2023-09-22 00:55:04 +0400

    Create Window

--- src/main.cpp
+++ src/main.cpp
@@ -32,10 +32,122 @@ constexpr const int RESULT_OK = 0;
 // MAIN
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+const int OPEN_GL_VERSION_MAJOR = 3;
+const int OPEN_GL_VERSION_MINOR = 0;
+GLFWwindow *mWindow;
+int mFrameBufferWidth = 0;
+int mFrameBufferHeight = 0;
+
+static void handleGLFWErrors(int error, const char* description)
+{
+  std::cout << "error_callback: " << description << "\n";
+}
+
+static void handleKeyEvents(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+  if (!mWindow)
+  {
+    return;
+  }
+
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+  {
+    glfwSetWindowShouldClose(mWindow, GLFW_TRUE);
+  }
+}
+
+void handleFramebufferSizeChange(GLFWwindow *pWindow, int width, int height)
+{
+  if (!mWindow)
+  {
+    return;
+  }
+
+  mFrameBufferWidth = width;
+  mFrameBufferHeight = height;
+  glViewport(0, 0, mFrameBufferWidth, mFrameBufferHeight);
+}
+
+bool onSurfaceReady()
+{
+  glfwGetFramebufferSize(mWindow, &mFrameBufferWidth, &mFrameBufferHeight);
+  glViewport(0, 0, mFrameBufferWidth, mFrameBufferHeight);
+
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+  return true;
+}
+
+bool init()
+{
+  if (!glfwInit())
+  {
+    std::cout << "init: failed to init\n";
+    return false;
+  }
+
+  glfwSetErrorCallback(handleGLFWErrors);
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, OPEN_GL_VERSION_MAJOR);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, OPEN_GL_VERSION_MINOR);
+  mWindow = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGL Triangle", nullptr, nullptr);
+  if (!mWindow)
+  {
+    std::cout << "init: failed to create window\n";
+    return false;
+  }
+
+  glfwSetKeyCallback(mWindow, handleKeyEvents);
+
+  glfwMakeContextCurrent(mWindow);
+  gladLoadGL(glfwGetProcAddress);
+
+  glfwSetFramebufferSizeCallback(mWindow, handleFramebufferSizeChange);
+  if (!onSurfaceReady())
+  {
+    std::cout << "init: failed to load assets\n";
+    return false;
+  }
+
+  return true;
+}
+
+void render()
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void mainLoop()
+{
+  while (!glfwWindowShouldClose(mWindow))
+  {
+    render();
+
+    glfwSwapBuffers(mWindow);
+    glfwPollEvents();
+  }
+}
+
+void terminate()
+{
+  if (mWindow)
+  {
+    glfwDestroyWindow(mWindow);
+  }
+
+  glfwTerminate();
+}
+
 int main()
 {
-  std::cout << "Hello World !\n";
-  std::cin.get();
+  if (init())
+  {
+    mainLoop();
+  }
+
+  terminate();
 
   return RESULT_OK;
 }
